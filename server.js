f35036572401172c92f3215eb9e8ebeab226f35b
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2023-08-07 12:50:34 -0500

    build express project

---
+++ server.js
@@ -0,0 +1,42 @@
+// import npm packages
+const express = require('express')
+const logger = require('morgan')
+const path = require('path')
+
+// import routers
+const indexRouter = require('./routes/index.js')
+
+// create the express app
+const app = express()
+
+// view engine setup
+app.set('view engine', 'ejs')
+
+// basic middleware
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// mount imported routes
+app.use('/', indexRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = app
